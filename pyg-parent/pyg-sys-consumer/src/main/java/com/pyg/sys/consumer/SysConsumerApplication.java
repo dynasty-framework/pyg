cde0419b53846421ac43dc39123e8b13f42a801c
zhaotao@yuanh.net <zhaotao@yuanh.net>
2019-05-27 23:45:23 +0800

    第一次提交

---
+++ pyg-parent/pyg-sys-consumer/src/main/java/com/pyg/sys/consumer/SysConsumerApplication.java
@@ -0,0 +1,17 @@
+package com.pyg.sys.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description: 运营商后台web层启动类
+ * @author: HP
+ * @date: 2019-05-27 22:57
+ */
+@SpringBootApplication
+public class SysConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SysConsumerApplication.class, args);
+    }
+}
